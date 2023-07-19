68ee871b5ff8f144fe3920d26f1f162a6c4c5730
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-07-18 23:33:02 -0400

    files

--- Python_SQL/test/connector.py
+++ Python_SQL/test/connector.py
@@ -2,3 +2,12 @@
 The file to make the connection to the sql server
 """
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
+
+print(mydb) 
